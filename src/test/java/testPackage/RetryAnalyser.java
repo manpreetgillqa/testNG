a5dbd6baef4c9a22294703470596b3bd78dfc86b
jazz <theautomationtechies@gmail.com>
2022-08-18 21:01:36 -0400

    Retry with screenshot

--- src/test/java/testPackage/RetryAnalyser.java
+++ src/test/java/testPackage/RetryAnalyser.java
@@ -1,7 +1,13 @@
 package testPackage;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
 
 public class RetryAnalyser implements IRetryAnalyzer {
     int counter = 0;
@@ -13,6 +19,7 @@ public class RetryAnalyser implements IRetryAnalyzer {
     public boolean retry(ITestResult iTestResult) {
         if(counter<retries){
             counter++;
+            testLogin.takeScreenshot(iTestResult.getMethod().getMethodName());
             return true;
         }
         return false;
--- src/test/java/testPackage/testLogin.java
+++ src/test/java/testPackage/testLogin.java
@@ -4,21 +4,23 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
-import java.util.logging.FileHandler;
-import java.util.random.RandomGenerator;
+
+import org.apache.commons.io.FileUtils;
 
 public class testLogin {
 
-    WebDriver driver;
+    static WebDriver driver;
 
 
     @Test
@@ -38,33 +40,42 @@ public class testLogin {
     }
 
 
-    @Test
-
-    public void test3() throws InterruptedException {
+    @Test(retryAnalyzer = RetryAnalyser.class)
+    public void testGoogle() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-
         driver.navigate().to("https://google.com/");
         Thread.sleep(2000);
-        Assert.assertEquals(true,false);
-
+        Assert.assertEquals(randomNumber(100)%2,0);
     }
 
     @AfterMethod
-    public void teardown(ITestResult result){
-        if(ITestResult.FAILURE==result.getStatus()){
-            TakesScreenshot sc = (TakesScreenshot) driver;
-            File scFile = sc.getScreenshotAs(OutputType.FILE);
-            File newFile = new File("./ScreenShot1.png");
-
-        }
-
+    public void teardown()  {
         driver.quit();
+    }
+
+    public static void takeScreenshot(String methodName){
+            try {
+                TakesScreenshot sc = (TakesScreenshot) driver;
+                File scFile = sc.getScreenshotAs(OutputType.FILE);
+                File newFile = new File("./screenShots/"+ methodName + "_"+ getTimeStamp()+ ".png");
+                FileUtils.copyFile(scFile,newFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
     }
     public static int randomNumber(int max){
         Random r = new Random();
         return r.nextInt(max);
+    }
+    public static String getTimeStamp(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        Long time = new Date().getTime();
+        Timestamp timestamp = new Timestamp(time);
+        return dateFormat.format(timestamp);
     }
 }
